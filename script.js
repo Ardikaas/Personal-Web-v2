d180d9098d93d81cdb94806540548a719d334529
Ardikaas <ardikaajisetiawan@gmail.com>
2023-04-07 14:25:39 +0700

    add animation and add simple header

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 function erorr(){
   alert('uh oh sowething wrong with it')
 }
+
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+    }
+  });
+}); 
+
+let hidden = document.querySelectorAll('.hidden');
+hidden.forEach((el) => observer.observe(el));
+
+var text = ["Ardikaas", "Amateur Designer", "Junior Web Designer"];
+var idx = 0;
+var ubah = document.getElementById('change-text');
+let anim = document.getElementsByClassName('show-text');
+var lama = setInterval(change, 6000)
+
+function change(){
+  ubah.innerHTML = text[idx];
+  idx++;
+  if(idx >= text.length){
+    idx = 0;
+  }
+}
